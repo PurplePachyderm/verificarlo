1041703352e2bd6e834d81bdfed89d988d48b5aa
Aurélien Delval <a.delval87@gmail.com>
2021-06-16 07:45:32 +0200

    Dummy commit to try to trigger Docker build

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-autoreconf -is
 
+# Deinit sigdigits submodule, if already present
+git submodule deinit -f src/tools/sigdigits
+rm -rf .git/modules/src/tools/sigdigits
+
+# (Re)-init submodules
 git submodule init
 git submodule update
+
+autoreconf -is
--- tests/paths.sh
+++ tests/paths.sh
@@ -1,1 +1,3 @@
 LLVM_BINDIR=/usr/lib/llvm-7/bin
+GCC_PATH=gcc
+FLANG_PATH=/usr/bin/flang
